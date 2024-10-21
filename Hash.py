9d733cf57a93cbe69ba255cb093c4d35f51b334a
Timmy <timmy@TC-Mac.lan>
2024-10-21 08:58:58 -0700

    done with part 1

--- Hash.py
+++ Hash.py
@@ -1,5 +1,6 @@
 from Crypto.Hash import SHA256
 from datetime import datetime
+import random
 
 
 def sha256_hash(input_string, bits):
@@ -22,77 +23,11 @@ def hamming_distance_bytes(str1, str2):
     return distance
 
 
-def get_partner_bytes(key, bits):
-    mask = 0b0001
-    lst = []
-    count = 0
-    while count < bits:
-        byte = key ^ mask  # 0b0001
-        print(f"key: {bin(key)[2:]}")
-        str = bits_to_string(byte)
-        lst.append(str)
-        mask = mask << 1
-        count += 1
-    return lst
-
-
-def bits_to_string(binary_literal, encoding='latin-1'):
-    bit_string = bin(binary_literal)[2:]
-    print(bit_string)
-    if len(bit_string) % 8 != 0:
-        bit_string = bit_string.zfill(len(bit_string) + (8 - len(bit_string) % 8))
-    byte_array = bytearray(int(bit_string[i:i + 8], 2) for i in range(0, len(bit_string), 8))
-    return byte_array.decode(encoding)
-
-
 def bits_str_to_string(bit_string, encoding='latin-1'):
     if len(bit_string) % 8 != 0:
         bit_string = bit_string.zfill(len(bit_string) + (8 - len(bit_string) % 8))
     byte_array = bytearray(int(bit_string[i:i + 8], 2) for i in range(0, len(bit_string), 8))
     return byte_array.decode(encoding)
-
-
-def find_str_collisions(bits):
-    count = 0
-    key = 0b0
-
-    while count < 2 ** bits:
-        lst = []
-        str_key = bits_to_string(key)
-        lst = get_partner_bytes(key, bits)
-        for char in lst:
-            m0 = str_key
-            m1 = char
-            print(f"Trying: {m0}, {m1}")
-            if check_collisions(m0, m1, bits):
-                return (m0, m1)
-        key += 1
-        count += 1
-
-
-def find_collisions(bits):
-    map = create_str_pairs(bits)
-    lst = []
-    for key, values in map.items():
-        m0 = key
-        for val in values:
-            m1 = val
-            print(f"Trying: {m0},{m1}")
-            if check_collisions(m0, m1, bits):
-                lst.append((m0, m1))
-    return lst
-
-
-def check_collisions(m0, m1, bits, case_count):
-    h1 = sha256_hash(m0, bits)
-    h2 = sha256_hash(m1, bits)
-    print(
-        f"m0,m1: {(m0, m1)}, h1: {h1}, h2: {h2}, Hamming Distance: {hamming_distance_bytes(m0, m1)}, case: {case_count}")
-
-    return h1 == h2
-
-
-import random
 
 
 def generate_random_binary_string(length):
@@ -101,55 +36,64 @@ def generate_random_binary_string(length):
 
 def flip_random_bit(binary_string):
     binary_list = list(binary_string)
-
     flip_index = random.randint(0, len(binary_list) - 1)
-
     binary_list[flip_index] = '1' if binary_list[flip_index] == '0' else '0'
-
     return ''.join(binary_list)
 
 
-def new_and_improved_find_collision(bits):
-    choices = [8, 16, 24, 32, 40, 48, 56, 64, 72, 80, 88]
+def check_collisions(m0, m1, bits, case_count, hash_map):
+    h1 = sha256_hash(m0, bits)
+    h2 = sha256_hash(m1, bits)
+    if h1 == h2:
+        return m0, m1
+    elif h1 in hash_map:
+        return m0, hash_map[h1]
+    elif h2 in hash_map:
+        return m1, hash_map[h2]
+    else:
+        hash_map[h1] = m0
+        hash_map[h2] = m1
+        return None
+
+
+def find_collisions(bits):
     case_count = 0
     start = datetime.now()
+    hash_map = {}
+    hash_set = set()
     while True:
-        index = random.randint(0, 10)
-        base_string = generate_random_binary_string(choices[index])
-        new_string = flip_random_bit(base_string)
-        print(f"\nm0 bin: {base_string}")
-        print(f"m1 bin: {new_string}")
+        index = random.randint(1, 100)
+        base_string = generate_random_binary_string(index)
         m0 = bits_str_to_string(base_string)
+        if m0 in hash_set:
+            continue
+        new_string = flip_random_bit(base_string)
         m1 = bits_str_to_string(new_string)
-        if check_collisions(m0, m1, bits, case_count):
+        if m1 in hash_set:
+            continue
+        hash_set.add(m0)
+        hash_set.add(m1)
+        result = check_collisions(m0, m1, bits, case_count, hash_map)
+        if result is not None:
+            m0, m1 = result
+            h0 = sha256_hash(m0, bits)
+            h1 = sha256_hash(m1, bits)
             end = datetime.now()
             time_diff = end - start
-            return (m0, m1, time_diff, case_count)
+            return m0, m1, time_diff, case_count, h0, h1
         case_count += 1
+        print(f"Bits: {bits} Attempt: {case_count}")
 
 
 def get_all_cases():
     bits = 8
     file = open("result.txt", "a")
     while bits < 51:
-        m0, m1, time, input_size = new_and_improved_find_collision(bits)
-        file.write(f"B i t s: {bits}\nm0: {m0}\nm1: {m1}\nTime: {time}\nInput_size: {input_size}\n\n\n")
+        m0, m1, time, input_size, h0, h1 = find_collisions(bits)
+        file.write(f"B i t s: {bits}\nm0: {m0}\nm1: {m1}\nh0: {h0}\nh1: {h1}\nTime: {time}\nAttempts: {input_size}\n\n\n")
         bits += 2
 
 
 if __name__ == "__main__":
-    # str1 = "a"
-    # str2= "b"
-    # print(hamming_distance_bytes(str1, str2))
-    # print(find_collisions(25))
-    # Example usage
-    # base_string = generate_random_binary_string(36)
-    # new_string = flip_random_bit(base_string)
-    # print(base_string)
-    # print(new_string)
-    # print(bits_str_to_string(base_string))
-    # print(bits_str_to_string(new_string))
-    # print(new_and_improved_find_collision(16))
     get_all_cases()
-
 
